6fb5d4ae18fe317c79f77c91c9151c89707fa7ff
Nicholas Cook <nicholas.cook@betsoftware.com>
2022-08-04 13:36:51 +0200

    feat: producer

--- Producer/Controllers/WeatherForecastController.cs
+++ Producer/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using Confluent.Kafka;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Producer.Controllers;
 
@@ -6,6 +8,16 @@ namespace Producer.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
+    // Kafka stuff
+
+    ProducerConfig _producerConfig = new ProducerConfig
+    {
+        BootstrapServers = "127.0.0.1:29092"
+    };
+
+    // End of Kafka stuff
+
+
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -18,15 +30,27 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet(Name = "SendWeatherForecast")]
+    public async Task<string> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        // Random rubbish data
+        var weather = Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+
+        // Kafka produce!
+        using (var producer = new ProducerBuilder<string, string>(_producerConfig).Build())
+        {
+            string json = JsonConvert.SerializeObject(weather);
+            var message = new Message<string, string> { Key = DateTime.Now.ToString("yyyyMMddHHmmss"), Value = json };
+
+            var deliveryResult = await producer.ProduceAsync("weather", message);
+
+            return deliveryResult.Status.ToString();
+        }
     }
 }
